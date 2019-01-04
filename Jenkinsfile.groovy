ac39a47c9ebe0e77e06083b2b252528207181577
kolomyya <andriyzn4@gmail.com>
2019-01-03 21:54:55 -0600

    jenkins files

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages{
+        stage("Git Clone"){
+            steps{
+                git 'git@github.com:kolomyya/coolwebsite_on_pipeline.git'
+            }
+        }
+        stage ("Copy file to web" ){
+            steps{
+                sh "cp -f index.html /var/www/html/index.html"
+            }
+        }
+    }
+}
